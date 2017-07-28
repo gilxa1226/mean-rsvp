9d0c710cc725b55272360e245c9691f08f04819c
gilxa1226 <mike.haller@gmail.com>
2017-07-28 11:16:31 -0400

    Completed Part 2

---
+++ src/app/auth/auth.config.ts
@@ -0,0 +1,19 @@
+import { ENV } from './../core/env.config';
+
+interface AuthConfig {
+  CLIENT_ID: string;
+  CLIENT_DOMAIN: string;
+  AUDIENCE: string;
+  REDIRECT: string;
+  SCOPE: string;
+  NAMESPACE: string;
+};
+
+export const AUTH_CONFIG: AuthConfig = {
+  CLIENT_ID: 'y4dBosCl7bozf4qRnwX3V0UqWd12WxQp',
+  CLIENT_DOMAIN: 'gilxa1226.auth0.com',
+  AUDIENCE: 'http://localhost:8083/api/',
+  REDIRECT: `${ENV.BASE_URI}/callback`,
+  SCOPE: 'openid profile',
+  NAMESPACE: 'http://myapp.com/roles'
+};
--- src/server/config.js
+++ src/server/config.js
@@ -2,5 +2,6 @@
 module.exports = {
   AUTH0_DOMAIN: 'gilxa1226.auth0.com', // e.g., kmaida.auth0.com
   AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://auth0:qwerty123456@ds151062.mlab.com:51062/mean_rsvp'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://auth0:qwerty123456@ds151062.mlab.com:51062/mean_rsvp',
+  NAMESPACE: 'http://myapp.com/roles'
 };
