020a5cbead82e703ee0007bb6e3a77e1e9976227
gilxa1226 <mike.haller@gmail.com>
2017-07-31 14:26:02 -0400

    Completed Part 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +11,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { AuthService } from './auth/auth.service';
+import { ApiService } from './core/api.service';
+import { UtilsService } from './core/utils.service';
+import { FilterSortService } from './core/filter-sort.service';
 import { CallbackComponent } from './pages/callback/callback.component';
+import { LoadingComponent } from './core/loading.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +23,22 @@ import { CallbackComponent } from './pages/callback/callback.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    CallbackComponent
+    CallbackComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     Title,
-    AuthService
+    AuthService,
+    ApiService,
+    DatePipe,
+    UtilsService,
+    FilterSortService
   ],
   bootstrap: [AppComponent]
 })
